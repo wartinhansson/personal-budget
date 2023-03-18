09dafdbdf42c508d59f35e8c92f71032a06cc407
Martin H <96685540+wartinhansson@users.noreply.github.com>
2023-03-18 17:29:56 +0100

    Use envelopesRouter in app + comments

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const express = require("express");
+const { envelopesRouter } = require("./envelopes");
 const app = express();
 
-const PORT = 3000;
+// Use envelopesRouter on /envelopes
+app.use("/envelopes", envelopesRouter);
 
+// Test get
 app.get("/", (req, res, next) => {
   res.send("Hello, World!");
 });
 
+// Start server on port 3000
+const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
