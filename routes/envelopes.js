b2e6e0ed609658dd0d7519cf3646a453a29310a8
Martin H <hansson.martin@icloud.com>
2023-04-08 18:20:58 +0200

    Update db.js with pure functions

--- routes/envelopes.js
+++ routes/envelopes.js
@@ -7,15 +7,26 @@ const {
   updateInstanceInDatabase,
   deleteInstanceFromDatabase,
 } = require("../db/db.js");
+const statement = {
+  table: "envelopes",
+  columns: ["envelope_label", "envelope_limit"],
+  id: "envelope_id",
+};
 
 // Check envelopeId Middleware
 envelopesRouter.param("envelopeId", async (req, res, next, envelopeId) => {
-  const envelope = await getFromDatabaseById(envelopeId);
+  // Statement values
+  statement.values = [envelopeId];
 
+  // Find in database
+  const envelope = await getFromDatabaseById(statement);
+
+  // If match then set req.envelope to match and go next
   if (envelope) {
     req.envelope = envelope;
     next();
   } else {
+    // Else send envelope not found
     res.status(404).send("Envelope not found.");
   }
 });
@@ -24,21 +35,30 @@ const LabelAndLimitMiddleware = (req, res, next) => {
   const label = req.body["envelope_label"];
   const limit = req.body["envelope_limit"];
 
+  // Check if limit is less than zero
   if (limit < 0) {
     res.status(400).send("envelope_limit must be greater than 0");
     return;
   }
 
-  if (label && limit) next();
-  else
+  // If both label and limit exist then set req parameters and next
+  if (label && limit) {
+    req.label = label;
+    req.limit = limit;
+
+    next();
+  } else {
+    // Else send bad request
     res
       .status(400)
       .send("Request body must contain envelope_label and envelope_limit");
+  }
 };
 
 // Get all budget envelopes
 envelopesRouter.get("/", async (req, res, next) => {
-  const envelopes = await getAllFromDatabase();
+  // Get all envelopes from database
+  const envelopes = await getAllFromDatabase(statement);
 
   res.send(envelopes);
 });
@@ -50,15 +70,20 @@ envelopesRouter.get("/:envelopeId", (req, res, next) => {
 
 // Create budget envelope
 envelopesRouter.post("/", LabelAndLimitMiddleware, async (req, res, next) => {
-  const envelope = await addToDatabase(req.body);
+  // Statement values
+  statement.values = [req.label, req.limit];
+
+  // Add to database
+  const envelope = await addToDatabase(statement);
 
   res.status(201).send(envelope);
 });
 
 // Transfer value from one envelope to another
 envelopesRouter.post("/transfer/:from/:to", async (req, res, next) => {
-  const from = await getFromDatabaseById(req.params.from);
-  const to = await getFromDatabaseById(req.params.to);
+  const query = "SELECT * FROM envelopes WHERE envelope_id = $1";
+  const from = await getFromDatabaseById(query, req.params.from);
+  const to = await getFromDatabaseById(query, req.params.to);
 
   if (!from) res.status(404).send("From envelope not found.");
   else if (!to) res.status(404).send("To envelope not found.");
@@ -91,10 +116,11 @@ envelopesRouter.put(
   "/:envelopeId",
   LabelAndLimitMiddleware,
   async (req, res, next) => {
-    const updatedInstance = await updateInstanceInDatabase(
-      req.envelope,
-      req.body
-    );
+    // Statement values
+    statement.values = [req.label, req.limit, req.envelope["envelope_id"]];
+
+    // Update in database
+    const updatedInstance = await updateInstanceInDatabase(statement);
 
     res.send(updatedInstance);
   }
@@ -102,7 +128,8 @@ envelopesRouter.put(
 
 // Delete a budget envelope
 envelopesRouter.delete("/:envelopeId", async (req, res, next) => {
-  const deleted = await deleteInstanceFromDatabase(req.envelope);
+  // Delete envelope from database
+  const deleted = await deleteInstanceFromDatabase(statement);
 
   if (deleted) res.status(204).send();
 });
